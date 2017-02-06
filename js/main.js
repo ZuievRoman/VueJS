94da2d5994b19dde298290282685e5b38d4e0b50
Roman <zuevro@gmail.com>
2017-02-06 21:48:08 +0200

    filters les-05

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
+Vue.filter('emoji', function (value) {
+    return value.replace('единорог', '&#129412;')
+        .replace('щенок', '&#128054;')
+        .replace('огне', '&#128293;')
+        .replace('грустно', '&#128546;')
+
+})
+
 new Vue({
     el: "#app",
     data:{
-        show: false
+        input: '',
+        array:[
+            {
+                name: 'Иван',
+                age: 45
+            },{
+                name: 'Вика',
+                age: 19
+            },{
+                name: 'Иван',
+                age: 29
+            },{
+                name: 'Максим',
+                age: 20
+            }
+        ]
     }
 });
