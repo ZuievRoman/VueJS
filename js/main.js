58389dc13f6a92cb6cef4e5e7b595c6a6b0f33fd
Roman <zuevro@gmail.com>
2017-02-06 20:56:59 +0200

    transition and v-if les-04

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 new Vue({
     el: "#app",
     data:{
-        message: ''
-    },
-    methods: {
-        onClick: function () {
-            console.log(this.message)
-        }
+        show: false
     }
 });
